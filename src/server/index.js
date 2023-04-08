420347337bfef9cc6f44f126960b0b4c558ebc05
Edmund0 <goziem.ofili@gmail.com>
2023-04-08 10:49:57 -0600

    Halted

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,14 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+
+var aylien = require("aylien_textapi");
+
+var textapi = new aylien({
+  application_id: "your-api-id",
+  application_key: "your-key"
+});
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -9,8 +17,8 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
