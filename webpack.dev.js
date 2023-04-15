bd0912e5c2bc195b4ffe3c38254db2bf8aae6508
Edmund0 <goziem.ofili@gmail.com>
2023-04-15 10:02:37 -0600

    RESTART

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,14 +11,9 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            },
-
-            {
-                test: /.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
