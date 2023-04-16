5c44193bac01d4020f118c6cc16af5358cf51c4a
Edmund0 <goziem.ofili@gmail.com>
2023-04-15 18:08:32 -0600

    hell

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,9 +11,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+
+            {
+                test: /.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
--- webpack.prod.js
+++ webpack.prod.js
@@ -8,9 +8,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+
+            {
+                test: /.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
